9f829d826eaef131efaee0bd408155ed8ae5d312
Erik Shandrowski <erik843@gmail.com>
2014-07-22 16:00:05 -0600

    Finished all including bonus

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ fs.readFile('data.txt', function(err, data) {
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/public/:filename', function(req, res) {
+	// console.log(fs.readFileSync('public/' + req.params.filename, 'utf-8'));
+	fs.readFile('public/' + req.params.filename, function(err, data) {
+		res.header('Content-Type', 'text/html');
+		res.send(data);
+	});
+})
 
 var server = app.listen(5666, function() {
 	console.log('Express server listening on port ' + server.address().port);
